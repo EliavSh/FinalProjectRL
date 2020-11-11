201f36233615c8e88db8e0c8354e465f8fc8f748
Eliav.Shalelashvili <eliav9896@gmail.com>
2020-11-11 22:59:27 +0200

    .

--- runnable_scripts/post_process.py
+++ runnable_scripts/post_process.py
@@ -4,7 +4,7 @@ import shutil
 from numpy import linspace
 
 
-dir_name = "800_episodes"
+dir_name = "mcts_vs_ddqn"
 
 # create rewards dir
 path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)), "results", dir_name, 'rewards')
@@ -15,7 +15,7 @@ if not os.path.exists(path):
 str_arr = []
 
 for size in range(2, 10, 2):
-    for exploration_rate in linspace(0.0, 4., 11):
+    for exploration_rate in linspace(0.0, 4., 21):
         str_arr.append('board_' + str(size) + '_exploration_' + str(round(exploration_rate, 2)))
 
 for root, dirs, files in os.walk(os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)), "results", dir_name)):
