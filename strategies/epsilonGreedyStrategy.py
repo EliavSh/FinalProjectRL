50fc9838b2d26b20f4688b41aa3738e5de40d2f9
ELIAV <eliav9896@gmail.com>
2021-02-02 22:56:47 +0200

    fixing test epsilon values

--- strategies/epsilonGreedyStrategy.py
+++ strategies/epsilonGreedyStrategy.py
@@ -8,11 +8,11 @@ class EpsilonGreedyStrategy(Strategy):
         self.end_learning_step = num_train_episodes * (zombies_per_episode + board_width + 2)
         # load strategy info
         self.start = float(strategy_info['eps_start'])
-        self.end = 0  # math.exp(int(strategy_info['eps_end']))
+        self.end = math.exp(int(strategy_info['eps_end']))
         self.decay = math.log(self.end / self.start, math.exp(1)) / self.end_learning_step
 
     def get_exploration_rate(self, current_step):
         if current_step > self.end_learning_step:
-            return self.end
+            return 0
         else:
             return self.start * math.exp(current_step * self.decay)
