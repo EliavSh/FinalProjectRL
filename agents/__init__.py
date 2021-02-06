d60b6ea89243c0b30b0561a129fd3d2d4e2e54ea
ELIAV <eliav9896@gmail.com>
2021-02-06 02:14:14 +0200

    1. adding init file to agents package 2. renamed random to uniform

---
+++ agents/__init__.py
@@ -0,0 +1,7 @@
+from agents.constant_agent import ConstantAgent
+from agents.double_constant_agent import DoubleConstantAgent
+from agents.uniform_agent import UniformAgent
+from agents.gaussian_agent import GaussianAgent
+from agents.ddqn_agent import DdqnAgent
+from agents.basic_mcts_agent import BasicMCTSAgent
+from agents.alphaZero.alpha_zero_agent import AlphaZeroAgent
--- agents/uniform_agent.py
+++ agents/uniform_agent.py
@@ -3,9 +3,9 @@ from agents.agent import Agent
 from strategies.epsilonGreedyStrategy import EpsilonGreedyStrategy
 
 
-class RandomAgent(Agent):
+class UniformAgent(Agent):
     def __init__(self, device, agent_type, config):
-        super(RandomAgent, self).__init__(agent_type, config)
+        super(UniformAgent, self).__init__(agent_type, config)
 
     def select_action(self, state):
         rate = self.strategy.get_exploration_rate(current_step=self.current_step)
